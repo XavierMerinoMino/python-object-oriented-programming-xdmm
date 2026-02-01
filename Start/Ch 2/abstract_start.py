d6cc3084463d38612d4abb5a1cc97b0162e2044e
Xavier <46076385+XavierMerinoMino@users.noreply.github.com>
2026-02-01 05:39:34 +0000

    2026-02-01-POO-Chapter1 and 2

--- Start/Ch 2/abstract_start.py
+++ Start/Ch 2/abstract_start.py
@@ -1,28 +1,52 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using Abstract Base Classes to enforce class constraints
 
+# abc: provides the infrastructure for defining Abstract Base Classes (ABCs)
+# ABC: A helper base class used to declare a class as an abstract base class. 
+# Inheriting from this class is the modern and simplest way to create an ABC.
+# abstractmethod: decorator used within an ABC to mark methods that must be 
+# implemented by any concrete subclass.
+from abc import ABC, abstractmethod
 
-class GraphicShape:
+class GraphicShape(ABC):
     def __init__(self):
         super().__init__()
 
+    @abstractmethod
     def calcArea(self):
         pass
 
+    @abstractmethod
+    def calcPerimeter(self):
+        pass
 
 class Circle(GraphicShape):
     def __init__(self, radius):
         self.radius = radius
 
+    def calcArea(self):
+        return 3.14 * (self.radius ** 2)
+    
+    def calcPerimeter(self):
+        return 2 * 3.14 * self.radius
 
 class Square(GraphicShape):
     def __init__(self, side):
         self.side = side
 
+    def calcArea(self):
+        return self.side * self.side
+    
+    def calcPerimeter(self):
+        return self.side * 4
 
-g = GraphicShape()
+# g = GraphicShape()
 
 c = Circle(10)
-print(c.calcArea())
+print(f"Perimeter: {c.calcPerimeter():.4f}")
+print(f"Area: {c.calcArea():.4f}")
+# print(c.calcArea())
 s = Square(12)
-print(s.calcArea())
+# print(s.calcArea())
+print(f"Perimeter: {s.calcPerimeter()}")
+print(f"Area: {s.calcArea():.4f}")
--- Start/Ch 2/interface_start.py
+++ Start/Ch 2/interface_start.py
@@ -1,6 +1,12 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using Abstract Base Classes to implement interfaces
 
+# Interfaces are not part of Python language
+# An interface is a blueprint or contract that defines a set of 
+# method signatures (what a class can do) without providing any 
+# implementation details (how it does it), forcing any class that 
+# implements it to provide its own concrete versions of those methods
+
 from abc import ABC, abstractmethod
 
 
@@ -12,14 +18,22 @@ class GraphicShape(ABC):
     def calcArea(self):
         pass
 
+# All the methods from this class must be abstract to be an interface.
+class JSONify(ABC):
+    @abstractmethod
+    def toJSON(self):
+        pass
 
-class Circle(GraphicShape):
+class Circle(GraphicShape, JSONify):
     def __init__(self, radius):
         self.radius = radius
 
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
 
+    def toJSON(self):
+        return f"{{'Circle': {str(self.calcArea())}}}"
 
 c = Circle(10)
 print(c.calcArea())
+print(c.toJSON())
